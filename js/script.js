0a6359a69455312f387b28d8e3052da78b727028
Riki420 <girottiriccardo1998@gmail.com>
2021-09-01 15:29:41 +0200

    for 10 in axios + print 10 email

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ const app = new Vue ({
     },
     method: {
 
-    }
-    
+    },
+    created() {
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                this.emailList.push(res.data.response)
+          });
+        } 
+      },
 })
