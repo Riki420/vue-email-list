e42eea8c0752b159be743d2a323fb008be04e04c
Riki420 <girottiriccardo1998@gmail.com>
2021-09-02 10:31:02 +0200

    sorted emailList

--- js/script.js
+++ js/script.js
@@ -5,10 +5,17 @@ const app = new Vue ({
     data: {
         emailList: []
     },
+    computed:{
+        sortedMails(){                                                                                      
+            //Funzione per ordinare l'array dalla A alla Z
+            return this.emailList.sort()
+        }
+    },
     method: {
 
     },
-    created() {
+    created() {                                                                                             
+        //Giro 10 volte per creare 10 mail da pushare nell'array
         for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
                 this.emailList.push(res.data.response)
